8ef7bf3aa7b3289d3c3aabb0a5e92db004a0300f
Michael-T55 <mic81070@gmail.com>
2024-04-27 17:08:20 +0100

    still on updated nav

--- src/Pages/Nav.jsx
+++ src/Pages/Nav.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-import navScroll from "../main";
 
 function Nav() {
 
   const [clicked, setClicked] = useState(false);
+  const [navbar, setNavbar] = useState(false);
 
 
 
@@ -13,11 +13,22 @@ function Nav() {
    setClicked(!clicked);
  };
 
+  const changeNavbarBg = () => {
+    // console.log(scrollY)
+    if (window.scrollY >= 50) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+  window.addEventListener('scroll', changeNavbarBg)
   return (
     <>
-      <div> 
+      <div>
         <nav
-          className={`navbar flex items-center p-2 justify-between px-4 sm:px-16 w-[100%] bg-fixed fixed z-10 top-0 dark:md:bg-transparent md:bg-transparent dark:bg-[#041644] bg-slate-700 ${navScroll}`}
+          className={`navbar flex items-center p-2 justify-between px-4 sm:px-16 w-[100%] bg-fixed fixed z-10 top-0 dark:md:bg-transparent md:bg-transparent dark:bg-[#041644] md:items-center bg-slate-700  ${
+            navbar ? "navbar active" : "navbar"
+          }`}
         >
           <div className="logo">
             <button>
@@ -26,7 +37,7 @@ function Nav() {
           </div>
           <div>
             <ul
-              className={`md:flex md:static md:gap-5 md:m-0 md:bg-transparent md:flex-row md:p-0 md:h-0 md:right-0 transition ease-in dark:bg-slate-900 bg-slate-200 h-[30rem] absolute rounded-xl p-5 flex flex-col gap-5  top-0 w-1/2 pt-10 -right-full m-2 ${
+              className={`md:relative md:gap-5 md:m-0 md:bg-transparent0 md:flex-row md:p-0 md:py-0 md:h-0 md:w-full md:items-center md:right-0 dark:bg-slate-900 bg-slate-200 h-[30rem] absolute rounded-xl p-5 flex flex-col gap-5  top-0 w-1/2 pt-10 -right-full m-2  ${
                 clicked ? "sideNav" : "nav-side"
               }`}
               onClick={handleClick}

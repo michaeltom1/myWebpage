22b7a3dec302cd14287bb8220991c94e0f8230f5
michaeltom1 <mic81070@gmail.com>
2024-10-26 20:36:59 +0100

    vercelPathFix3

---
+++ src/Home.jsx
@@ -0,0 +1,22 @@
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Hero from "./components/home/Hero";
+import About from "./components/home/About";
+import Services from "./components/home/Services";
+import Projects from "./components/home/Projects";
+import Contact from "./components/home/Contact";
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <About />
+      <Services />
+      <Projects />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
